107383d7d0af7797a9a3eb017596ec0233276bd8
hareth-labadi <hareth-labadi@etu.usthb.dz>
2024-04-29 14:26:53 +0100

    first commit

--- components/Goals.jsx
+++ components/Goals.jsx
@@ -12,12 +12,12 @@ const Goals = () => {
     <section className="flex flex-wrap lg:flex-row justify-around gap-12 xl:gap-0 items-center bg-silver mb-10">
       <Image src={"/Goal.png"} alt="Goals" width={400} height={280} />
       <div className="max-w-[600px]">
-        <h2 className="text-primary mb-4">
+        <h2 className="text-primary mb-4 px-4">
           Our Goals
         </h2>
         <ul className="text-xl text-primary self-start list-inside">
           {goals.map((goal, index) => (
-            <li className="relative circle-check custom-list mb-2" key={goal}>
+            <li className="relative circle-check custom-list mb-2 px-4" key={goal}>
               • {goal}
             </li>
           ))}
